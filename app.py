73ea11d68428a5d0dcd101bc86f77a6330c6ec92
Jihye <jhye207@gmail.com>
2023-03-20 01:24:25 +0900

    로그인 기능

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template 
+#render_template: HTML 파일 렌더링 
 
 app = Flask(__name__)
 
+#서버
 @app.route("/")
-def Test():
-    return "Test"
+def Hello():
+    return "Sejong-ChatBot"
+
+#GET: url에 파라미터 함께 넘김
+#POST: http 내부에 데이터 추가(보안)
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        uid = request.form.get('uid')
+        upw = request.form.get('upw')
+        print(uid, upw)
+        return "Post success"
 
 if __name__ == '__main__':
     app.debug = True
