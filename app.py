bb88a17fdac8821e74bfe4b2bd66b57a12366287
Jihye <jhye207@gmail.com>
2023-04-04 09:55:39 +0900

    CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #main
 from api import create_app, socketio 
-
+from flask_cors import CORS
 app = create_app(debug = True)
+CORS(app)
+
 # app run
 if __name__ == '__main__':
     socketio.run(app)
