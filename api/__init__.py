15e1ba4f823a362e9f79841759baa8f2b5f5317e
Jihye <jhye207@gmail.com>
2023-04-13 03:59:47 +0900

    DB connect

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 import os
 from dotenv import load_dotenv
+from . import db
+
 load_dotenv()
 
 socketio = SocketIO(logger=True, engineio_logger=True)
@@ -11,6 +13,9 @@ socketio = SocketIO(logger=True, engineio_logger=True)
 def create_app(debug=False):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    #DB connect
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from api.events import ChatNamepsace
     socketio.on_namespace(ChatNamepsace('/chat'))
@@ -19,5 +24,10 @@ def create_app(debug=False):
     app.register_blueprint(main_blueprint)
 
     socketio.init_app(app)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- api/routes.py
+++ api/routes.py
@@ -1,11 +1,12 @@
-# 서버
+#server
 from flask import session, render_template, request, redirect, url_for, Blueprint
 from sejong_univ_auth import ClassicSession, auth
+from .db import db, User
 
 main = Blueprint('main', __name__)
 main.secret_key = "secretkey"  # for session
 
-# main
+#main
 @main.route("/")
 def start():
     if "uid" in session:
@@ -13,33 +14,76 @@ def start():
         return redirect(url_for("main.chat"))
     else:
         return redirect(url_for("main.login"))
-
-# 로그인 서버
+'''
+#test code
+#login page
 @main.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'GET':
-        return  {"uid": "id",
-                 "upw": "password"}
+        return render_template("login.html")
+    elif request.method == 'POST':
+        uid = request.form.get("uid")
+        upw = request.form.get("upw")
+        #auth(id: str, password: str, methods: Authenticator)
+        result = auth(id=uid, password=upw, methods=ClassicSession)
+        if result.is_auth:  #login success
+            record = User.query.filter_by(id=uid).first()
+            if record:
+                pass
+            else:
+                name = result.body['name']
+                major = result.body['major']
+                grade = result.body['grade']
+                user = User(uid, name, major, grade)
+                db.session.add(user)
+                db.session.commit()
+            if "uid" not in session:
+                session["uid"] = uid
+            return redirect(url_for("main.chat"))
+        else:
+            return redirect(url_for("main.login"))
+        
+#chatbot page
+@main.route("/chat")    
+def chat():
+    user = session.get("uid", "")
+    if user == "":
+        return redirect(url_for("main.login"))
+    return render_template("chat.html", user = user)
 
+'''
+
+#login page
+@main.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("index.html")
     elif request.method == 'POST':
         uid = request.get_json("uid")
         upw = request.get_json("upw")
         #auth(id: str, password: str, methods: Authenticator)
         result = auth(id=uid, password=upw, methods=ClassicSession)
         if result.is_auth:
-            session["uid"] = uid
-            print("uid:" + session["uid"])
+            record = User.query.filter_by(id=uid).first()
+            if record:
+                pass
+            else:
+                name = result.body['name']
+                major = result.body['major']
+                grade = result.body['grade']
+                user = User(uid, name, major, grade)
+                db.session.add(user)
+                db.session.commit()
+            if "uid" not in session:
+                session["uid"] = uid
             return redirect(url_for("main.chat"))
         else:
             return redirect(url_for("main.login"))
         
-# 채팅 서버
-@main.route("/chat")
+#chatbot page
+@main.route("/chat")    
 def chat():
     user = session.get("uid", "")
     if user == "":
         return redirect(url_for("main.login"))
-#    return render_template('chat.html', user = user)
-    return {"uid":"학번",
-        "umsg":"user input",
-        "bmsg":"bot output"}
+    return render_template("index.html", user = user)
