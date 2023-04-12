46e742964b9b0dbe2cf51b6127bb8dc92bc583cb
Jihye <jhye207@gmail.com>
2023-04-13 03:23:32 +0900

    Configuration settings

--- api/__init__.py
+++ api/__init__.py
@@ -1,21 +1,23 @@
-#app 초기 설정용
-#key, DB connect
+#app initial setting
+#key, DB connect (secret key)
 from flask import Flask
 from flask_socketio import SocketIO
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 socketio = SocketIO(logger=True, engineio_logger=True)
 
-def create_app(debug=True):
+def create_app(debug=False):
     app = Flask(__name__)
-    app.debug = True
-    app.config['SECRET_KEY'] = 'password'
-
-    socketio.init_app(app)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     from api.events import ChatNamepsace
     socketio.on_namespace(ChatNamepsace('/chat'))
 
     from api.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    socketio.init_app(app)
 
     return app
