c524e203b127a9fc07b58c76ad14b2ab93fcf8b4
Jihye <jhye207@gmail.com>
2023-04-26 04:51:47 +0900

    chore: modify access token config

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-#from datetime import timedelta
 from . import db
 from dotenv import load_dotenv
 
@@ -17,11 +16,12 @@ def create_app(debug=False):
     app = Flask(__name__)  
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-    ACCESS_EXPIRES = timedelta(hours=1) #access token: 1hour
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = ACCESS_EXPIRES
+
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=int(os.getenv('ACCESS_EXPIRES')))
+#    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=int(os.getenv('REFRESH_EXPIRES')))
 
     jwt = JWTManager(app)
-    
+
     #DB connect
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
