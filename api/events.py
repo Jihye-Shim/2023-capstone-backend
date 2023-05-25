b14293fe34ad31a0125b86dcee8253668832bd0a
Jihye <jhye207@gmail.com>
2023-05-26 03:26:19 +0900

    refactor: modify connected model

--- api/events.py
+++ api/events.py
@@ -2,8 +2,8 @@
 from flask_socketio import emit, join_room, leave_room, Namespace
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from .db import User, Log
-from .model import predict
 import datetime
+from api import chatmodel
 
 class ChatNamepsace(Namespace):
 
@@ -42,10 +42,11 @@ class ChatNamepsace(Namespace):
     def on_sendreply(self, data):
         uid = get_jwt_identity()
         user = User.query.filter_by(id=uid).first()
-        answer = predict(str(data["input"]), user.major)
-        log = str(answer).replace(' / ', '\n')
+        answer = chatmodel.get_prediction(uid[:2], str(data["input"]), user.major)
+#        log = str(answer).replace(' / ', '\n')
+        log = str(answer)
         emit('reply', {"msg": log}, room=user.id)
         time = datetime.datetime.now()
-        Log.save_log(uid, self.input[uid], log, self.input_time[uid], time, None, None)
+        Log.save_log(uid, self.input[uid], log, self.input_time[uid], time, None, None, None)
         del self.input[uid]
         del self.input_time[uid]
